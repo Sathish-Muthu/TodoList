31a42dd49fbafdd3a744805b1058def194cbecba
sathish.m <sathish.m@changepond.com>
2023-02-12 11:16:25 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sathish M on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
